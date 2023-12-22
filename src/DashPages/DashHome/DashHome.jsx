fac576a45cfd652387b1a2b84f934a3fcbdc9fa7
swadebsharma6 <swadebriva2012@gmail.com>
2023-12-22 19:55:53 +0600

    added final push

--- src/DashPages/DashHome/DashHome.jsx
+++ src/DashPages/DashHome/DashHome.jsx
@@ -1,9 +1,11 @@
 
 
 const DashHome = () => {
+
+
     return (
-        <div>
-            <h2>DashHome</h2>
+        <div className="flex justify-center items-center min-h-screen">
+            <h2 className="text-4xl text-center font-bold"> <span className="text-primary">WelCome  To </span>Task Management Application</h2>
         </div>
     );
 };
--- src/DashPages/Ongoing.jsx
+++ src/DashPages/Ongoing.jsx
@@ -13,7 +13,7 @@ const Ongoing = () => {
             setOngoing(data)
         })
 
-    }, [])
+    }, []);
 
     return (
         <div>
